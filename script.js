c4a29f6928d1c9d46c9d9c7a4da119c3d668aad2
AyushChaursiaya <147719677+AyushChaursiaya@users.noreply.github.com>
2023-12-24 09:12:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const extendHex = (shortHex) => {
   // write your code here
+	let i = shortHex.length - 3;
+	let isCapital = false;
+	let output = "#";
+	for (let i = 0; i < shortHex.length; i++) {
+		let asciiCode = shortHex[i].charAt(i);
+		if (asciiCode >= 97 && asciiCode <= 122) {
+			isCapital = true;
+		}
+		output += shortHex[i] + shortHex[i];
+		i++;
+	}
 };
 
+return isCapital ? output.toUpperCase() : output;
 // Do not change the code below.
-const shortHex = prompt("Enter Short Hex.");
-alert(extendHex(shortHex));
+// const shortHex = prompt("Enter Short Hex.");
+// alert(extendHex(shortHex));
